ff24b0dc774f1687a6f5284260c9a77d056fa274
paulovictorln <paulovictor494@gmail.com>
2018-07-01 23:44:08 -0300

    Métodos de callback

---
+++ src/main/java/br/jus/util/AnimalPers.java
@@ -0,0 +1,35 @@
+package br.jus.util;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
+import br.jus.hibernate.model.Animal;
+
+public class AnimalPers {
+
+	public static void main(String[] args) {
+		EntityManager manager = JpaUtil.getEntityManager();
+		EntityTransaction tx = manager.getTransaction();
+		tx.begin();
+		Calendar dataNascimento = Calendar.getInstance();
+		dataNascimento.set(2011, 2, 1);
+
+		Animal animal = new Animal();
+		animal.setNome("Mimosa");
+		animal.setDataNascimento(dataNascimento.getTime());
+		animal.setDataUltimaAtualizacao(new Date());
+		
+		System.out.println("Idade antes de persistir: " + animal.getIdade());
+		manager.persist(animal);
+		
+		System.out.println("Idade depois de persistir: " + animal.getIdade());
+		
+		tx.commit();
+		manager.close();
+		JpaUtil.close();
+	}
+
+}
