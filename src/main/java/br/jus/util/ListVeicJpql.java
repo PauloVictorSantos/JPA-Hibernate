af352f1359143c26d3091df490445643996b539c
paulovictorln <paulovictor494@gmail.com>
2018-07-04 01:12:38 -0300

    Paginação setando o numero de registro por página e navegação

--- src/main/java/br/jus/util/ListVeicJpql.java
+++ src/main/java/br/jus/util/ListVeicJpql.java
@@ -1,6 +1,7 @@
 package br.jus.util;
 
 import java.util.List;
+import java.util.Scanner;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -13,15 +14,34 @@ public class ListVeicJpql {
 
 		EntityManager manager = JpaUtil.getEntityManager();
 		String jpql = "from Veiculo";
-		
-		TypedQuery<Veiculo> query = manager.createQuery(jpql,Veiculo.class);
-		query.setFirstResult(0);
-		query.setMaxResults(10);
-		
-		List<Veiculo> veiculos = query.getResultList();
-		System.out.println(veiculos.size()+" itens");
-		for (Veiculo veiculo : veiculos) {
-			System.out.println(veiculo.getModelo() + " " + veiculo.getFabricante() + ": " + veiculo.getAnoFabricacao());
-		}
+
+		System.out.print("Registros por página: ");
+
+		Scanner scanner = new Scanner(System.in);
+		int registrosPorPagina = scanner.nextInt();
+		int numeroDaPagina = 0;
+
+		TypedQuery<Veiculo> query = manager.createQuery(jpql, Veiculo.class);
+
+		do {
+			System.out.print("Número da página: ");
+			numeroDaPagina = scanner.nextInt();
+			if (numeroDaPagina != 0) {
+				int primeiroRegistro = (numeroDaPagina - 1) * registrosPorPagina;
+				query.setFirstResult(primeiroRegistro);
+				query.setMaxResults(registrosPorPagina);
+				List<Veiculo> veiculos = query.getResultList();
+				System.out.println(veiculos.size() + " itens");
+
+				for (Veiculo veiculo : veiculos) {
+					System.out.println(
+							veiculo.getModelo() + " " + veiculo.getFabricante() + ": " + veiculo.getAnoFabricacao());
+				}
+
+			}
+		} while (numeroDaPagina != 0);
+		scanner.close();
+		manager.close();
+		JpaUtil.close();
 	}
 }
