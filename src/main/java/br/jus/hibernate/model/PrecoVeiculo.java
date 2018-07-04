6503899662d559a33e9d9847b057374505756926
paulovictorln <paulovictor494@gmail.com>
2018-07-04 01:32:30 -0300

    consultas usando o operador new

---
+++ src/main/java/br/jus/hibernate/model/PrecoVeiculo.java
@@ -0,0 +1,31 @@
+package br.jus.hibernate.model;
+
+import java.math.BigDecimal;
+
+public class PrecoVeiculo {
+	private String modelo;
+	private BigDecimal valor;
+
+	public PrecoVeiculo(String modelo, BigDecimal valor) {
+		super();
+		this.modelo = modelo;
+		this.valor = valor;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+
+	public void setModelo(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public BigDecimal getValor() {
+		return valor;
+	}
+
+	public void setValor(BigDecimal valor) {
+		this.valor = valor;
+	}
+
+}
--- src/main/java/br/jus/util/ListVeicJpql.java
+++ src/main/java/br/jus/util/ListVeicJpql.java
@@ -5,21 +5,22 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
 
-import br.jus.hibernate.model.Veiculo;
+import br.jus.hibernate.model.PrecoVeiculo;
 
 public class ListVeicJpql {
 
 	public static void main(String[] args) {
 
 		EntityManager manager = JpaUtil.getEntityManager();
-		String jpql = "select modelo from Veiculo";
-		
-		TypedQuery<String> query = manager.createQuery(jpql,String.class);
-		
-		List<String> modelos = query.getResultList();
+		String jpql = "select new br.jus.hibernate.model. PrecoVeiculo(modelo, valor) from Veiculo";
 
-		for (String modelo : modelos) {
-			System.out.println(modelo);
+		TypedQuery<PrecoVeiculo> query = manager.createQuery(jpql, PrecoVeiculo.class);
+
+		List<PrecoVeiculo> resul = query.getResultList();
+
+		for (PrecoVeiculo preco : resul) {
+
+			System.out.println(preco.getModelo() + " - " + preco.getValor());
 		}
 	}
 }
