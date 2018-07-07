b5b9a662ba4bb2c36e34f9f84c1694dc63804db1
paulovictorln <paulovictor494@gmail.com>
2018-07-06 21:17:30 -0300

    Lista de Object[]

---
+++ src/main/java/br/jus/util/FuncaoAgreCriteriaApi.java
@@ -0,0 +1,31 @@
+package br.jus.util;
+
+import java.math.BigDecimal;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+
+import br.jus.hibernate.model.Veiculo;
+import br.jus.util.JpaUtil;
+
+public class FuncaoAgreCriteriaApi {
+
+	public static void main(String[] args) {
+		EntityManager manager = JpaUtil.getEntityManager();
+		CriteriaBuilder builder = manager.getCriteriaBuilder();
+		CriteriaQuery<BigDecimal> criteriaQuery = builder.createQuery(BigDecimal.class);
+
+		Root<Veiculo> veiculo = criteriaQuery.from(Veiculo.class);
+		criteriaQuery.select(builder.sum(veiculo. <BigDecimal> get("valor")));
+		
+		TypedQuery<BigDecimal> query = manager.createQuery(criteriaQuery);
+		BigDecimal total =query.getSingleResult();
+		
+		System.out.println("Valor total"+total);
+		
+	}
+
+}
--- src/main/java/br/jus/util/ListObjectCriteriaApi.java
+++ src/main/java/br/jus/util/ListObjectCriteriaApi.java
@@ -1,5 +1,6 @@
+package br.jus.util;
 
-import java.math.BigDecimal;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.persistence.TypedQuery;
@@ -8,23 +9,22 @@ import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
 
 import br.jus.hibernate.model.Veiculo;
-import br.jus.util.JpaUtil;
 
-public class FuncaoAgreCriteriaApi {
+public class ListObjectCriteriaApi {
 
 	public static void main(String[] args) {
 		EntityManager manager = JpaUtil.getEntityManager();
 		CriteriaBuilder builder = manager.getCriteriaBuilder();
-		CriteriaQuery<BigDecimal> criteriaQuery = builder.createQuery(BigDecimal.class);
-
+		CriteriaQuery<Object[]> criteriaQuery = builder.createQuery(Object[].class);
 		Root<Veiculo> veiculo = criteriaQuery.from(Veiculo.class);
-		criteriaQuery.select(builder.sum(veiculo. <BigDecimal> get("valor")));
+		criteriaQuery.multiselect(veiculo.<String>get("modelo"), veiculo.<String>get("valor"));
+
+		TypedQuery<Object[]> query = manager.createQuery(criteriaQuery);
+		List<Object[]> resultado = query.getResultList();
 		
-		TypedQuery<BigDecimal> query = manager.createQuery(criteriaQuery);
-		BigDecimal total =query.getSingleResult();
-		
-		System.out.println("Valor total"+total);
-		
+		for(Object[] valores:resultado){
+			System.out.println(valores[0]+" - "+valores[1]);
+		}
 	}
 
 }
